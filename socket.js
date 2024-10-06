41da3439c54a439f11b5d7c03325eaa635db0dfa
cbys10 <cbysket@gmail.com>
2024-10-06 13:50:40 +0000

    stuff

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 const io = new Server();
 
 io.on("connection", (socket) => {
-  
+  console.log(`Client Connected, SID:${socket.sid}`)
 });
 
 io.listen(3000);

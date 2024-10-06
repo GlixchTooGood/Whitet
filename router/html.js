9657dee931e40fb242e590a9bef4a1adc489eb6f
cbys10 <cbysket@gmail.com>
2024-10-06 17:53:47 +0000

    some new css

--- router/html.js
+++ router/html.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+router.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+})
 
+router.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+})
 
-
-
+router.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'views', 'register.html'));
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./router/api");
 const htmlRouter = require("./router/html");
+const path = require('path');
 app.use(apiRouter);
 app.use(htmlRouter);
 app.engine('html', require('ejs').renderFile);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('static'));
-const port = 443;
+const port = 9993;
 const host = '0.0.0.0';
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+})
 app.listen(port,host,()=>{
     console.log(`Server up on host:${host} on port:${port}`)
 })
